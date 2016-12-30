7efa0ad4110304b8cc708bbb8e85c4cee4a69dc9
Gareth Jones <gareth.jones@shinetech.com>
2016-12-30 15:32:39 +1100

    copied over changes from log4js, added linting, bumped version

--- test/date_format-test.js
+++ test/date_format-test.js
@@ -1,42 +1,60 @@
-"use strict";
-var should = require('should')
-, dateFormat = require('../lib');
+'use strict';
 
-describe('date_format', function() {
-  var date = new Date(2010, 0, 11, 14, 31, 30, 5);
+require('should');
+const dateFormat = require('../lib');
 
-  it('should format a date as string using a pattern', function() {
-    dateFormat.asString(dateFormat.DATETIME_FORMAT, date).should.eql("11 01 2010 14:31:30.005");
+function createFixedDate() {
+  return new Date(2010, 0, 11, 14, 31, 30, 5);
+}
+
+describe('date_format', () => {
+  const date = createFixedDate();
+
+  it('should default to now when a date is not provided', () => {
+    dateFormat.asString(dateFormat.DATETIME_FORMAT).should.not.be.empty();
   });
 
-  it('should default to the ISO8601 format', function() {
+  it('should be usable directly without calling asString', () => {
+    dateFormat(dateFormat.DATETIME_FORMAT, date).should.eql('11 01 2010 14:31:30.005');
+  });
+
+  it('should format a date as string using a pattern', () => {
+    dateFormat.asString(dateFormat.DATETIME_FORMAT, date).should.eql('11 01 2010 14:31:30.005');
+  });
+
+  it('should default to the ISO8601 format', () => {
     dateFormat.asString(date).should.eql('2010-01-11 14:31:30.005');
   });
 
-  it('should provide a ISO8601 with timezone offset format', function() {
-    date.getTimezoneOffset = function() { return -660; };
-    dateFormat.asString(
-      dateFormat.ISO8601_WITH_TZ_OFFSET_FORMAT, 
-      date
-    ).should.eql(
-      "2010-01-11T14:31:30+1100"
-    );
+  it('should provide a ISO8601 with timezone offset format', () => {
+    let tzDate = createFixedDate();
+    tzDate.setMinutes(tzDate.getMinutes() - tzDate.getTimezoneOffset() - 660);
+    tzDate.getTimezoneOffset = function () {
+      return -660;
+    };
 
-    date.getTimezoneOffset = function() { return 120; };
-    dateFormat.asString(
-      dateFormat.ISO8601_WITH_TZ_OFFSET_FORMAT, 
-      date
-    ).should.eql(
-      "2010-01-11T14:31:30-0200"
-    );
+    dateFormat.asString(dateFormat.ISO8601_WITH_TZ_OFFSET_FORMAT, tzDate).should.eql('2010-01-11T14:31:30.005+1100');
+
+    tzDate = createFixedDate();
+    tzDate.setMinutes((tzDate.getMinutes() - tzDate.getTimezoneOffset()) + 120);
+    tzDate.getTimezoneOffset = function () {
+      return 120;
+    };
+
+    dateFormat.asString(dateFormat.ISO8601_WITH_TZ_OFFSET_FORMAT, tzDate).should.eql('2010-01-11T14:31:30.005-0200');
   });
 
-  it('should provide a just-the-time format', function() {
+  it('should provide a just-the-time format', () => {
     dateFormat.asString(dateFormat.ABSOLUTETIME_FORMAT, date).should.eql('14:31:30.005');
   });
 
-  it('should provide a custom format', function() {
-    date.getTimezoneOffset = function() { return 120; };
-    dateFormat.asString("O.SSS.ss.mm.hh.dd.MM.yy", date).should.eql('-0200.005.30.31.14.11.01.10');
+  it('should provide a custom format', () => {
+    const customDate = createFixedDate();
+    customDate.setMinutes((customDate.getMinutes() - customDate.getTimezoneOffset()) + 120);
+    customDate.getTimezoneOffset = function () {
+      return 120;
+    };
+
+    dateFormat.asString('O.SSS.ss.mm.hh.dd.MM.yy', customDate).should.eql('-0200.005.30.31.14.11.01.10');
   });
 });
